329b6c1ead4e832ddd8315b18dbe9bcc60fd5341
Marc Munene <marcmunene104@gmail.com>
2025-02-26 13:55:52 +0300

    changes

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@_$%*";
 const spaces = " ";
+
+const getRandom = () => {
+  const index = Math.floor(Math.random() * chars.length);
+  return chars[index];
+};
+
+const generatePassword = () => {
+  const passwordInput = document.getElementById("password").value;
+  const lowercaseCheckbox = document.getElementById("lowercase");
+  const uppercaseCheckbox = document.getElementById("uppercase");
+  const numbersCheckbox = document.getElementById("numbers");
+  const symbolsCheckbox = document.getElementById("symbols");
+  const excludeDuplicateCheckbox = document.getElementById("exclude-duplicate");
+  const spacesCheckbox = document.getElementById("spaces");
+};
